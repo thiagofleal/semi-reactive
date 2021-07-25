312ab7cb433fcd9be02cada18abfdc53254215de
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2021-07-25 15:31:43 -0300

    Add Request class

---
+++ request.js
@@ -0,0 +1,76 @@
+export default class Request
+{
+	constructor(beforeFetch, catchFetchReturn) {
+		this.__beforeFetch = beforeFetch ? beforeFetch : () => {};
+		this.__catchFetch = catchFetch ? catchFetch : () => {};
+	}
+
+	async request(url, method, body, args) {
+		if (args === null || args === undefined) {
+			args = {};
+		}
+		args.method = method;
+		
+		if (body) {
+			args.body = JSON.stringify(body);
+		}
+		this.__beforeFetch(args);
+
+		const ret = await fetch(url, args);
+
+		this.__catchFetchReturn(ret);
+		return ret;
+	}
+
+	async getResponse(url, args) {
+		return await this.request(url, "GET", null, args);
+	}
+
+	async get(url, args) {
+		const response = await this.getResponse(url, args);
+		
+		if (response !== false) {
+			return await response.json();
+		}
+		return false;
+	}
+
+	async postResponse(url, body, args) {
+		return await this.request(url, "POST", body, args);
+	}
+
+	async post(url, body, args) {
+		const response = await this.postResponse(url, body, args);
+		
+		if (response !== false) {
+			return await response.json();
+		}
+		return false;
+	}
+
+	async putResponse(url, body, args) {
+		return await this.request(url, "PUT", body, args);
+	}
+
+	async put(url, body, args) {
+		const response = await this.putResponse(url, body, args);
+		
+		if (response !== false) {
+			return await response.json();
+		}
+		return false;
+	}
+
+	async deleteResponse(url, body, args) {
+		return await this.request(url, "DELETE", body, args);
+	}
+
+	async delete(url, body, args) {
+		const response = await this.deleteResponse(url, body, args);
+		
+		if (response !== false) {
+			return await response.json();
+		}
+		return false;
+	}
+}
