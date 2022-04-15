c31fb29c2358d0b75986006f969802c6448ccd32
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2022-04-14 22:34:23 -0300

    feat: Create Service class

--- core/service.js
+++ core/service.js
@@ -1,3 +1,5 @@
+import { Observable } from "./rx.js"
+
 export class Injectable
 {
     static services = [];
@@ -5,11 +7,14 @@ export class Injectable
     static register(classRef, ...args) {
         const ref = Injectable.services.find(i => i.classRef === classRef);
         const value = new classRef(...args);
-
+        value.onRegister();
+        
         if (ref) {
             ref.value = value;
+            value.notify("inject-set");
         } else {
             Injectable.services.push({ classRef, value });
+            value.notify("inject-create");
         }
     }
 
@@ -17,8 +22,38 @@ export class Injectable
         const item = Injectable.services.find(i => i.classRef === classRef);
         
         if (item) {
+            item.value.onGet();
             return item.value;
         }
         return null;
+    }
+}
+
+export class Service
+{
+    constructor() {
+        this.clients = [];
+    }
+
+    onRegister() {}
+
+    onGet() {}
+
+    notify(event) {
+        this.clients.forEach(client => client.next(event));
+    }
+
+    notifications() {
+        return new Observable(observer => {
+            this.clients.push(observer);
+
+            return () => {
+                const index = this.clients.indexOf(observer);
+
+                if (index !== -1) {
+                    this.clients.splice(index, 1);
+                }
+            };
+        });
     }
 }
