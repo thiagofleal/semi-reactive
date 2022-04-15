48412e83d972202d804542c04b6d91325412d14c
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2022-04-15 11:43:26 -0300

    revert: Remove TextComponent from components

--- core/components.js
+++ core/components.js
@@ -382,38 +382,3 @@ export class Switch extends Component
 		}
 	}
 }
-
-export class TextComponent extends Component
-{
-	constructor(props) {
-		if (props === undefined || props === null) {
-			props = {};
-		}
-		props.default = "";
-		super(props);
-	}
-
-	getText(property) {
-		if (property === undefined || property === null) {
-			property = "default";
-		}
-		return this[property];
-	}
-
-	setText(property, text) {
-		if (text === undefined || text === null) {
-			text = property;
-			property = "default";
-		}
-		this[property] = '' + text;
-	}
-
-	setControls(controls) {
-		this.definePropertiesObject(controls);
-	}
-
-	render() {
-		const property = this.getAttribute("control") || "default";
-		return this[property];
-	}
-}
