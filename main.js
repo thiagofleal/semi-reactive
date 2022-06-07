9d49d76bdbf3929094260f60395c678b49a2771f
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2022-06-06 21:58:40 -0300

    feat: Update main.js

--- main.js
+++ main.js
@@ -2,28 +2,38 @@ import { Injectable } from "./core/service.js";
 
 export class SemiReactive
 {
-	static start(args) {
+	static async start(args) {
 		let component = args.component;
 		const target = args.target;
 		const load = args.load;
 		const inject = args.inject;
 
-		if (component && target) {
-			component.show(target);
-		}
 		if (target && load) {
-			load.then(loaded => {
-				loaded.show(target);
-			});
+			const loaded = await load;
+			loaded.show(target);
 		}
 		if (Array.isArray(inject)) {
 			for (const service of inject) {
 				if (service instanceof Promise) {
-					service.then(Class_ => Injectable.register(Class_));
+					Injectable.register(await service);
 				} else {
 					Injectable.register(service);
 				}
 			}
+		}
+		if (component && target) {
+			let instance;
+
+			try {
+				instance = new component();
+			} catch (e) {
+				if (typeof component === "function") {
+					instance = component();
+				} else {
+					instance = component;
+				}
+			}
+			instance.show(target);
 		}
 	}
 
@@ -37,5 +47,9 @@ export class SemiReactive
 		return module => {
 			return module.default;
 		}
+	}
+
+	static instanceRoot(className, ...args) {
+		return () => new className(...args);
 	}
 };
