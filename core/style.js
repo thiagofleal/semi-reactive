e981ae559163f5ddd6ab3de645ec7a7c3549afed
Thiago Fernandes Leal <thiagofernandesleal@yahoo.com.br>
2022-09-15 22:48:55 -0300

    feat : New implementation of CSS in JS

--- core/style.js
+++ core/style.js
@@ -1,131 +1,22 @@
-function stackTrace() {
-	const e = new Error();
-	if (!e.stack) try {
-		throw e;
-	} catch (e) {
-		if (!e.stack) {
-			return [];
-		}
-	}
-	const stack = e.stack.toString().split(/\r\n|\n/);
-	stack.pop();
-	return stack;
-}
-
-const styles = new Map();
-
 export class Style {
-	static createName(length) {
-		const chars = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
-		let ret = "";
-
-		for (let i = 0; i < length; i++) {
-			ret += chars.charAt(Math.ceil(Math.random() * chars.length));
-		}
-		return ret;
+	static minify(content) {
+		return content.replace(/\n/g, "")
+			.replace(/\/\*.*?\*\//g, "")
+			.replace(/\s+/g, " ")
+			.replace(/\s\}/g, "}").replace(/\}\s/g, "}")
+			.replace(/\s\{/g, "{").replace(/\{\s/g, "{")
+			.replace(/\s\]/g, "]").replace(/\]\s/g, "]")
+			.replace(/\s\[/g, "[").replace(/\[\s/g, "[")
+			.replace(/\s:/g, ":").replace(/:\s/g, ":");
 	}
 
-	static __createStyle(css, selector, id) {
-		if (!id) {
-			id = selector.replace(/[^a-zA-Z\d\s:]/gi, '').replace(/\s/g, '');
-		}
-		let style = document.head.querySelector(`style#${id}`);
-
-		if (!style) {
-			style = document.createElement('style');
-			style.id = id;
-			document.head.append(style);
-		}
-		style.innerHTML = css.trim().split('&').map(element => {
-			if (element && element.length) {
-				return (selector + element).trim();
-			}
-			return "";
-		}).join('\n');
-	}
-
-	static create(selector, css, id) {
-		Style.__createStyle(css, selector, id);
-	}
-
-	static createClass(css) {
-		const stack = stackTrace();
-		const src = stack[stack.length - 1];
-
-		if (styles.has(src)) {
-			return styles.get(src);
-		}
-		const name = Style.createName(20);
-		Style.create("." + name, css);
-		styles.set(src, name);
-		return name;
-	}
-}
-
-export class StyledComponent {
-	constructor() {
-		this.__selector = "";
-	}
-
-	show(selector) {
-		this.__selector = selector;
-		this.reload();
-	}
-
-	getSelector() {
-		return this.__selector;
-	}
-
-	style() {
-		return '';
-	}
-
-	callBeforeReload() { }
-
-	reload() {
-		const selector = this.getSelector();
-		const style = this.style();
-		if (selector && style) {
-			Style.create(selector, style, this.constructor.name);
-		}
-	}
-}
-
-export class StylePlugin {
-	constructor(name) {
-		this.name = name;
-	}
-
-	apply(selector) {
-		const style = this.__style();
-		const elements = document.querySelectorAll(selector);
-
-		if (elements) {
-			elements.forEach(element => {
-				this.applyElement(element, style);
+	static create(css, prefix, posfix) {
+		const value = Style.minify(css)
+			.replace(/(\}|^|\{)[a-zA-Z0-9\-\s\[\],:\(\)*>="\.~\^\+]*(\{)/g, str => {
+				return str.replace(/.+?(?=,|\{)/g, select => `${prefix}${select}${posfix}`);
 			});
-		}
-	}
-
-	applyElement(element, style) {
-		if (!style) {
-			style = this.__style();
-		}
-		element.className = [
-			...element.className.split(' ').filter(n => n !== style),
-			style
-		].join(' ');
-	}
-
-	style() {
-		return '';
-	}
-
-	__style() {
-		return Style.createClass(this.style());
-	}
-
-	getName() {
-		return this.name;
+		const style = document.createElement("style");
+		style.innerHTML = value;
+		document.head.appendChild(style);
 	}
 }
